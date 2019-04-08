fb7a22d82b91dd22edd1a75091c8451bca0544fe
DallasO <8305525+DallasO@users.noreply.github.com>
2019-04-08 11:26:07 -0700

    Updates

---
+++ precache-manifest.7388ed1de0e7906e199c2ad9b2e603b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66d9727b0caa41a6b351",
+    "url": "/react-first-time/static/js/runtime~main.60e63276.js"
+  },
+  {
+    "revision": "36a54cc14b801b42958e",
+    "url": "/react-first-time/static/js/main.248e0c38.chunk.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react-first-time/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "36a54cc14b801b42958e",
+    "url": "/react-first-time/static/css/main.56be68b4.chunk.css"
+  },
+  {
+    "revision": "60f386cc78a339f88f708586c1ac72d9",
+    "url": "/react-first-time/index.html"
+  }
+];
